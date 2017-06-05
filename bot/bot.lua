5d738aa301a957d82f4150605f44903f8f2314e6
AmirHosseinKazemi <amirmntmntmnt@gmail.com>
2017-06-05 04:35:30 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {377028791},
+    sudo_users = {386587685},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
